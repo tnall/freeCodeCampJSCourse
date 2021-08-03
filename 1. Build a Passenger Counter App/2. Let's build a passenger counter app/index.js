c8d0e06ddd63a7120d2a4b2d86e8a8fa999c9e92
Timothy Nall <tnall@ihmc.org>
2021-08-03 09:44:50 -0700

    added test code

--- 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
+++ 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
@@ -2,7 +2,10 @@ let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
+
+
 function increment() {
+    console.log("The button was clicked.")
     count += 1
     countEl.textContent = count
 }
@@ -15,3 +18,5 @@ function save() {
 }
 
 console.log("Let's count people on the subway!")
+
+
