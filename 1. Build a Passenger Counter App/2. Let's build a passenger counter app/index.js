098788e75d84ef3837fb30161458dc98cac836c2
Timothy Nall <tnall@ihmc.org>
2021-08-04 11:54:10 -0700

    End of Part 1 of 6

--- 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
+++ 1. Build a Passenger Counter App/2. Let's build a passenger counter app/index.js
@@ -1,21 +1,30 @@
-let count = 0
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+let count = 0;
+let runningCount = 0;
+let countEL = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
+let welcomeEL = document.getElementById("welcome-el");
+let myName = "Tim";
+let greeting = "I missed you.";
+
+welcomeEL.textContent = welcomeEL.textContent + ", " + myName + ". " + greeting;
+
+//add emoji
+welcomeEL.textContent = welcomeEL.innerText + "❤️‍🔥";
 
 
-
-function increment() {
-    console.log("The button was clicked.")
-    count += 1
-    countEl.textContent = count
+function increment(){
+    count += 1;
+    //set countEl's inner text to count
+    countEL.textContent = count;
+}
+function save(){
+runningCount += count;
+    saveEL.textContent = "Previous entries: " + runningCount;
+    //reset the count
+    countEL.textContent = 0;
+    count = 0;
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
-    count = 0
-}
 
 console.log("Let's count people on the subway!")
 
